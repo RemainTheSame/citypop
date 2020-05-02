b02090c4f00a2f6486eade50df0787224de76c59
RemainTheSame <halooscur.orava@gmail.com>
2020-05-02 21:38:25 +0200

    Comments added to functions

--- src/components/SearchByComponent.js
+++ src/components/SearchByComponent.js
@@ -4,24 +4,27 @@ import React from "react"
 
 SearchByComponent
 
-
+- Search Field
+- Form submission
+- One component for both possible search selections
 
  */
 class SearchByComponent extends React.Component{
+    //Shows
     constructor(props){
         super()
         this.state = {
             currentSelection: props.selection,
             textInput:""
         }
-
-
     }
 
+    //Updates input field
     handleInput=(event)=>{
         this.setState({textInput: event.target.value})
     }
 
+    //Called when search form is submitted
     handleSearch(){
         console.log("Search done")
     }
